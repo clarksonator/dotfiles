dae8174778d3f3baded996604e883a893c2ce271
Nic Clarkson <clarksonator@gmail.com>
2024-10-26 22:38:40 -0600

    eos dotfiles

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -g fish_greeting
+    alias ls="eza --icons=always -la"
+    alias cat="bat --color always"
+    alias vim="nvim"
+    alias sv="sudo nvim"
+    alias man="batman"
+    alias vhc="nvim ~/dotfiles/hypr/.config/hypr/hyprland.conf"
+end
